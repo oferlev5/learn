2cea335e38fdeb83afa06188626a39d264003648
Ofer Lev <oferlev5@outlook.co.il>
2023-06-06 13:12:56 +0300

    work

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def foo():
+    print("hi")
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -18,3 +21,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
