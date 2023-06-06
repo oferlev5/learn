036408fcb6c4167487c7f34052ab332a093ef130
shahar128 <shahar128@gmail.com>
2023-06-06 13:07:44 +0300

    hello ofer

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     ofer = 4
     shahr = 5
+    x = 2
 
 
 # Press the green button in the gutter to run the script.
